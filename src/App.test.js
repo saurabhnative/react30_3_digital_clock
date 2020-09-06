05a673c8a0c86ab9adae570439671643b7dc7ada
codeclassifiers <codeclassifiers@gmail.com>
2020-09-06 11:08:19 +0530

    updated test cases

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders digital clock text', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = getByText(/Digital Clock/i);
+  expect(textElement).toBeInTheDocument();
 });
