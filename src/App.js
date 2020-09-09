1bef2d72dd96251b5601a7f70f7a4bad128cadf4
Saurabh Native <codeclassifiers@gmail.com>
2020-09-09 21:31:08 +0530

    starter template

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
-  const [time, updateTime] = useState(new Date());
-  useEffect(()=> {
-    const timer = setInterval(() => {
-      updateTime(new Date());
-    }, 1000);
-    return () => clearInterval(timer);
-  },[])
   return (
     <div className="App">
       <div className="elementcontainer">
         <h1>Digital Clock</h1>
-        <div className="timeparent">
-          <div className="timecontainer">
-            {/* print the string prettily */}
-            <span className="time">
-            {time.toLocaleTimeString()}
-            </span>
-          </div>
-         </div>
-      </div>
-      <div className="footer">
-        <div>References: </div>
-        <div className="firstlink"> 
-          <a href="https://reactjsexample.com/glowing-orb-clock-made-with-react/">Reactjs Examples</a>
-        </div>
-        <span>, </span>
-        <div className="firstlink">
-        <a href="https://stackoverflow.com/questions/44920162/how-to-display-working-digital-clock-using-react">Stack Overflow</a>
-        </div>
       </div>
     </div>
   );
